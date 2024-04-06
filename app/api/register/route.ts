e80c7f590f142120d39c2fa1403d324900b77c3b
Psng Abu <abupsngdscz@gmail.com>
2024-04-06 23:23:15 +0530

    added minor change in register api

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
-import prisma from "@/app/libs/prismadb";
+import client from "@/app/libs/prismadb";
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
 
   const hashedPassword = await bcrypt.hash(password, 12);
 
-  const user = await prisma.user.create({
+  const user = await client.user.create({
     data: {
       email,
       name,
