0cc9ee01e6acd030d4163b8909a1391a83994d0b
Psng Abu <abupsngdscz@gmail.com>
2024-04-07 08:41:37 +0530

    d

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,24 +1,21 @@
 import bcrypt from "bcrypt";
+
 import client from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
+export async function POST(request: Request) {
   const body = await request.json();
   const { email, name, password } = body;
 
   const hashedPassword = await bcrypt.hash(password, 12);
 
-  try {
-    const user = await client.user.create({
-      data: {
-        email,
-        name,
-        hashedPassword,
-      },
-    });
+  const user = await client.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
 
-    return Promise.resolve(NextResponse.json(user));
-  } catch (error) {
-    return Promise.resolve(NextResponse.error(error, 500)); // Adjust the status code as needed
-  }
+  return NextResponse.json(user);
 }
